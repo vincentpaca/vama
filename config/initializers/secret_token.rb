a943ed5f44cbef14b085a5d984f28402dcd6d448
Vincent Paca <vincent.paca@gmail.com>
2013-02-03 03:18:12 +0800

    Added initial rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vama::Application.config.secret_token = '2c3761f78af8b047e74fa6398aca7ff8e0d02fc079b99ec3453fd8e054087bbaa56ad785cf68228b98ecb3a6a66e04f406e73cda4fe25dc6ad6ba6051381edd8'
