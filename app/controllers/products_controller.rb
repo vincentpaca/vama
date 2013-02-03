4786d2d3ee9a3685c672e13bcd0c532271f33479
Vincent Paca <vincent.paca@gmail.com>
2013-02-03 13:39:11 +0800

    Added products navigation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,20 @@
 class ProductsController < ApplicationController
+  def index
+    if params[:model].present?
+      @title = Category.find(params[:model]).name
+      @products = Product.active.where(:category_id => params[:model])
+    else
+      @title = "Products"
+      @products = Product.active
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ActiveRecord::Base
-  attr_accessible :category_id, :cylinders, :engine, :facebook_link, :model, :name, :power, :facebook_album_link
+  attr_accessible :category_id, :cylinders, :engine, :facebook_link, :model, :name, :power, :facebook_album_link, :reserved
 
   belongs_to :category
+
+  def self.active
+    self.where(:reserved => false)
+  end
 end
