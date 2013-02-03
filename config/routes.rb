fc2d6cd3e87a6bbe7d4c7ac19ac057c48813a911
Vincent Paca <vincent.paca@gmail.com>
2013-02-03 12:39:18 +0800

    Added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Vama::Application.routes.draw do
   resources :dashboard
   resources :profiles
 
+  get :company_profile, :controller => :home, :action => :company_profile
+  get :show_room, :controller => :home, :action => :show_room
+  get :service_center, :controller => :home, :action => :service_center
+  get :faq, :controller => :home, :action => :faq
+  get :contact_us, :controller => :home, :action => :contact_us
+
   ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
