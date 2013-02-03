3aa1fec5180bbf2bc536c70c3c0e982d60c7ed63
Vincent Paca <vincent.paca@gmail.com>
2013-02-03 11:37:29 +0800

    Updated sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :load_categories
+
+  protected
+
+  def load_categories
+    @categories = Category.all
+  end
 end
