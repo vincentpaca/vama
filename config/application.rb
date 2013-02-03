9e27f00a99643b64c3aafa37180637555d2f97e1
Vincent Paca <vincent.paca@gmail.com>
2013-02-03 11:57:57 +0800

    Precompile activeadmin assets

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,7 @@ module Vama
     config.assets.version = '1.0'
 
     config.assets.initialize_on_precompile = false
+
+    config.assets.precompile += %w( active_admin.js active_admin.css )
   end
 end
