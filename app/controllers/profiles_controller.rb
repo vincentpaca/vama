b1bce89a6f433a5757522f8b30aa90c35b824af2
Vincent Paca <vincent.paca@gmail.com>
2013-02-03 11:09:19 +0800

    Added profile views

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,30 @@
-class ProfilesController < ApplicationController
+class ProfilesController < DashboardController
+  before_filter :check_ownership, :only => [ :show, :edit ]
+
+  def index
+    redirect_to "/dashboard"
+  end
+
+  def show
+    redirect_to edit_profile_path(current_user.profile)
+  end
+
+  def edit
+    @profile = current_user.profile
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  protected
+
+  def check_ownership
+    begin
+      redirect_to "/dashboard", :alert => "You don't have permission to access that profile" if Profile.find(params[:id]) != current_user.profile
+    rescue
+      redirect_to "/dashboard", :alert => "You don't have permission to access that profile"
+    end
+  end
 end
