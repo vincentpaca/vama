1a15bbc85023d4d970e7969dafdcd04c3ab7fd1a
Vincent Paca <vincent.paca@gmail.com>
2013-02-03 12:01:44 +0800

    Precompile print.css

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,6 @@ module Vama
 
     config.assets.initialize_on_precompile = false
 
-    config.assets.precompile += %w( active_admin.js active_admin.css )
+    config.assets.precompile += %w( active_admin.js active_admin.css.scss print.css )
   end
 end
