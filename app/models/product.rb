187e78cdffc3b43d0abf99fcdb05d021dcddcfbd
Vincent Paca <vincent.paca@gmail.com>
2013-02-03 13:10:10 +0800

    Added missing accessor

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :category_id, :cylinders, :engine, :facebook_link, :model, :name, :power
+  attr_accessible :category_id, :cylinders, :engine, :facebook_link, :model, :name, :power, :facebook_album_link
 
   belongs_to :category
 end
