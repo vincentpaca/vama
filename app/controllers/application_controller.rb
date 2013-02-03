3a8a98253268236ccc0c8a1d873b5c2686f100db
Vincent Paca <vincent.paca@gmail.com>
2013-02-03 13:47:14 +0800

    Fix errors with activeadmin and filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   require 'active_admin_pages_base.rb'
-
-  before_filter :load_categories
-
-  protected
-
-  def load_categories
-    @categories = Category.all
-  end
 end
