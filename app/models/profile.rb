c82f10efe58fa4913bfcc4d42280344108b823db
Vincent Paca <vincent.paca@gmail.com>
2013-02-03 10:20:20 +0800

    Removed profiles scaffold

--- app/models/profile.rb
+++
@@ -1,5 +0,0 @@
-class Profile < ActiveRecord::Base
-  attr_accessible :address, :contact_number, :full_name, :user
-
-  belongs_to :user
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Vama::Application.routes.draw do
   resources :profiles
-
+  resources :dashboard
 
   ActiveAdmin.routes(self)
-
   devise_for :admin_users, ActiveAdmin::Devise.config
-
   devise_for :users
 
   root :to => 'home#index'
