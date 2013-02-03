c032b8a416d5096ea3187b71dd5d31b042740b2e
Vincent Paca <vincent.paca@gmail.com>
2013-02-03 15:53:39 +0800

    Added missing field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130203075106) do
+ActiveRecord::Schema.define(:version => 20130203075250) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -77,8 +77,9 @@ ActiveRecord::Schema.define(:version => 20130203075106) do
   create_table "reservations", :force => true do |t|
     t.integer  "user_id"
     t.integer  "product_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+    t.boolean  "approved",   :default => false
   end
 
   create_table "users", :force => true do |t|
