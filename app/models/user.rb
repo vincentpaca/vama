5dc42f36756c841b1e2bf79ddaa4a6cd1181b807
Vincent Paca <vincent.paca@gmail.com>
2013-02-03 10:59:14 +0800

    Added profile relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  has_one :profile
+
+  after_create :build_profile
+
+  private
+
+  def build_profile
+    Profile.create!(:user => self)
+  end
 end
